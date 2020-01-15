f2494bbe0a66bc754c094c57a4e09a83d66b10a1
Rodolfo Echenique <rechenique@nubicom.com.ar>
2020-01-15 12:56:53 -0300

    build executed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { APP_ROUTING } from './app.routes';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { RegactComponent } from './pages/regact/regact.component';
+import { ComcamComponent } from './pages/comcam/comcam.component';
+import { PlanillaComponent } from './pages/planilla/planilla.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegactComponent,
+    ComcamComponent,
+    PlanillaComponent,
+    EstadisticasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
